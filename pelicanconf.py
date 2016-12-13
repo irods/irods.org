5948f16ba388cbec685d148c174428dbcf391f3e
Terrell Russell <terrellrussell@gmail.com>
2016-12-13 13:10:55 -0500

    add partners page redirect

--- pelicanconf.py
+++ pelicanconf.py
@@ -43,6 +43,7 @@ STATIC_PATHS = ['extras','images','uploads']
 
 EXTRA_PATH_METADATA = {
     'extras/.htaccess':   {'path': '.htaccess'},
+    'extras/partners.html':   {'path': 'partners/index.html'},
 }
 
 # No feeds while in development, requires absolute SITEURL (done in production)
